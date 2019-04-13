3d73158c52b49bbde8c1d20290e6bab905cc7885
JedenZhan <msygzzy1998@gmail.com>
2019-04-13 09:35:34 +0800

    19-4-13

--- 代码技能/React/src/App.js
+++ 代码技能/React/src/App.js
@@ -2,11 +2,17 @@ import React from 'react'
 import './style/index.styl'
 
 class App extends React.Component {
-
+    constructor () {
+        super();
+        this.state = {
+            num: 1
+        }
+    }
     render () {
         return (
             <div>
                 <button>hello world</button>
+                {this.state.num}
                 <img src='../assets/1.png' />
             </div>
         )
@@ -19,6 +25,15 @@ class App extends React.Component {
     }
     componentDidMount() {
         console.log('did mount')
+        this.setState({
+            num: this.state.num + 1,
+        })
+        this.setState({
+            num: this.state.num + 1,
+        })
+        this.setState({
+            num: this.state.num + 1,
+        })
     }
     componentWillUpdate() {
         console.log('will update')
